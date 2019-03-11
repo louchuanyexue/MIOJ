64ef3c1d409c57b32cc23b1b11ef93d937ff9632
LouChuanYeXue <1520355655@qq.com>
2019-03-11 22:34:59 +0800

    common update

---
+++ #1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <ctype.h>
+
+int main()
+{
+    // please write your code here
+    long a,b;
+    scanf("%ld %ld",&a,&b);
+    printf("%ld",a+b);
+    return 0;
+}
